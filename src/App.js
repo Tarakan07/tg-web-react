77614adfc88f1a3a0b1aa27bd1aeb947e2e7e290
Tarakan07 <boris_galunga@mail.ru>
2023-10-29 15:42:10 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
 	const { tg, onToggleButton } = useTelegram();
 	useEffect(() => {
 		tg.ready();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
