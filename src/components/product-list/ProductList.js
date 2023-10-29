48b17914e05bf6f0dab6435e857d43b90f26aabe
Tarakan07 <boris_galunga@mail.ru>
2023-10-29 18:14:39 +0300

    fix fetch

--- src/components/product-list/ProductList.js
+++ src/components/product-list/ProductList.js
@@ -39,7 +39,7 @@ const ProductList = () => {
 			totalPrice: getTotalPrice(addedItems),
 			queryId,
 		};
-		fetch("http://localhost:8000", {
+		fetch("http://localhost:8000/data", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
