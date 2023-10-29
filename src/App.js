d3bce475f67716dc398e061b4d5be8a58d42bad6
Tarakan07 <boris_galunga@mail.ru>
2023-10-29 15:36:54 +0300

    useTelegram

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+import { useTelegram } from "./hooks/useTelegram";
+
 const App = () => {
-	return <div>Hi sadas</div>;
+	const { tg, onToggleButton } = useTelegram();
+	useEffect(() => {
+		tg.ready();
+	}, []);
+
+	return (
+		<div>
+			<p>Hi sadas </p>
+			<button onClick={onToggleButton}>toggle </button>
+		</div>
+	);
 };
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../button/button";
+import "./header.css";
+import { useTelegram } from "../../hooks/useTelegram";
+const Header = () => {
+	const { onClose, user } = useTelegram();
+	return (
+		<div className={"header"}>
+			<Button onClick={onClose}>Закрыть</Button>
+			<span className={"username"}>{user?.username}</span>
+		</div>
+	);
+};
+
+export default Header;
